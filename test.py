7886836bf076621de41b2fa85d5da8c46a9998a5
jasmehakKaur <69070570+jasmehakKaur@users.noreply.github.com>
2021-09-20 15:36:03 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
- 
-import streamlit as st
-import time
-import datetime
-from datetime import datetime, date, time
-
-st.header("Official Date Picker")
-st.date_input('start date')
-st.date_input('end date')
