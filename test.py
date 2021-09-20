d5f8b7fcaf75f213d00c35b09c61bc76280f6683
jasmehakKaur <69070570+jasmehakKaur@users.noreply.github.com>
2021-09-20 12:43:41 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+ 
+import streamlit as st
+import time
+import datetime
+from datetime import datetime, date, time
+
+st.header("Official Date Picker")
+st.date_input('start date')
+st.date_input('end date')
